9edab395d955af74b5ee239b6af656f6ac9cc716
Alex Knutson <alexknutson@gmail.com>
2014-08-31 02:12:58 -0500

    clean up upload file

--- upload.php
+++ upload.php
@@ -1,19 +1,17 @@
 <?php
-var_dump($_FILES);
-$ds          = DIRECTORY_SEPARATOR;  //1
- 
-$storeFolder = 'images/uploads';   //2
- 
-if (!empty($_FILES)) {
-     
-    $tempFile = $_FILES['file']['tmp_name'];          //3             
-      
-    $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;  //4
-     
-    $targetFile =  $targetPath. $_FILES['file']['name'];  //5
- 
-    move_uploaded_file($tempFile,$targetFile); //6
-     
-}
+  // Handle screenshot uploads
+  $ds          = DIRECTORY_SEPARATOR;
+  $storeFolder = 'images/uploads';
+  if (!empty($_FILES)) {
+
+    $tempFile = $_FILES['file']['tmp_name'];
+
+    $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;
+
+    $targetFile =  $targetPath. $_FILES['file']['name'];
+
+    move_uploaded_file($tempFile,$targetFile); 
+
+  }
 ?>
 
